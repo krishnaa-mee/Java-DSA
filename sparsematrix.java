6b7257b16c61b9b3de6086a361b62bea646d1ae9
Krishna <ks2565525@gmail.com>
2026-08-26 23:33:24 +0530

    completed the question

--- sparsematrix.java
+++ sparsematrix.java
@@ -10,5 +10,28 @@ public class sparsematrix {
                 arr[i][j] = sc.nextInt();
             }
         }
+        System.out.println("display matrix");
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        int zerocount = 0;
+//        COUNT ZERO
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]==0){
+                    zerocount++;
+                }
+            }
+        }
+
+        if(zerocount > (m*n)/2){
+            System.out.println("Sparse Matrix");
+        }
+        else{
+            System.out.println("Not Sparse Matrix");
+        }
     }
 }
