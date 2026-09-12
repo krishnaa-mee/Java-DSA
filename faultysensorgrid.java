d2b22cd989736c147fd94d32eb0b2d3b1e3b88a7
Krishna <ks2565525@gmail.com>
2026-09-12 10:12:36 +0530

    contest question

---
+++ faultysensorgrid.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class faultysensorgrid {
+
+    static void gridzeros(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] || col[j]) {
+                    grid[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] grid = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        gridzeros(grid);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
